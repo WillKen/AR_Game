52219695ca7b8b077831fc6105bda7e6e115ec6b
zhangwk <501352226@qq.com>
2019-12-23 01:56:02 +0800

    upload pro

---
+++ AR_Game/Assets/VirtualButtonEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vb;
+    public Animator ani;
+
+    void Start()
+    {
+        //注册事件处理器
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        //在虚拟按钮中注册TrackableBehaviour事件
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+    
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        ani.SetTrigger("attack");
+        Debug.Log("Button Pressed!");  
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        ani.SetTrigger("wait");
+        Debug.Log("Button Released!");
+    }
+}
